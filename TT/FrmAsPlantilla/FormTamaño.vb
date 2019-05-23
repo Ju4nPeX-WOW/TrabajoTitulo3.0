dc8f50c082e0a464f8b94f1791c15ee552818a82
kevincillo98 <kevin.peralta@sansano.usm.cl>
2019-05-23 02:12:13 -0400

    Correccion : para evitar cambios en location

--- TT/FrmAsPlantilla/FormTamaño.vb
+++ TT/FrmAsPlantilla/FormTamaño.vb
@@ -1,3 +1,5 @@
 ﻿Public Class FormTamaño
-
+    Private Sub FormTamaño_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Location = New System.Drawing.Point(150, 45)
+    End Sub
 End Class
